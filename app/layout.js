84b203b32a73e8e8c51db07eb158e432b7167e95
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-07-21 13:58:59 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8804622562841633"
-     crossorigin="anonymous"></script></head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8804622562841633"
+     crossorigin="anonymous"></script>
       <body className={inter.className}>{children}</body>
     </html>
   );
