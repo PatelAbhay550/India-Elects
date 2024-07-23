57a9eb48eb9530f8254c303ac6583275a51afb6e
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-07-23 19:15:31 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,14 +13,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-XPYFXNJYNV"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-XPYFXNJYNV');
-</script><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8804622562841633" crossorigin="anonymous"></script></head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XPYFXNJYNV"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XPYFXNJYNV');
+          `}
+        </script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8804622562841633" crossorigin="anonymous"></script></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
