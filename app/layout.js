ce656a925edf9f54a3d09f2c792739e3eaea7da8
Abhay Patel <90134639+PatelAbhay550@users.noreply.github.com>
2024-07-23 20:20:16 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -11,9 +11,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-    <head>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XPYFXNJYNV"></script>
+   <html lang="en">
+      <head>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TFF6HJBZ');</script>
+        
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XPYFXNJYNV"></script>
         <script>
           {`
             window.dataLayer = window.dataLayer || [];
@@ -22,8 +28,13 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-XPYFXNJYNV');
           `}
         </script>
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8804622562841633" crossorigin="anonymous"></script></head>
-      <body className={inter.className}>{children}</body>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8804622562841633" crossorigin="anonymous"></script>
+      </head>
+      <body className={inter.className}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TFF6HJBZ"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {children}
+      </body>
     </html>
   );
 }
